590008b7e7c70f07c9907b2e68b2dfbc6ed8143c
Saksham Jaiswal <sjais973@gmail.com>
2021-08-30 01:27:20 +0530

    icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Header from './Header';
 function App() {
   return (
     <div className="app">
-     <h3>This is Facebook Clone</h3>
+      <Header/>
+
     </div>
   );
 }
