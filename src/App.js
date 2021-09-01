1b0971938f23c9af21f72c05c0df48b18dfeb0d1
Saksham Jaiswal <sjais973@gmail.com>
2021-09-01 21:35:02 +0530

    firebase

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
 import Widget from './Widget';
+import Login from './Login';
 function App() {
+  const user= null
   return (
     <div className="app">
-      <Header/>
+      {!user ? <Login/> :  <>   <Header/>
 <div className="app__body">
 <Sidebar/>
 <Feed/>
 <Widget/>
-</div>
+</div> 
+</> }
+ 
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyDw7q0n6cvCQjXpecMISaBqGwaCPD8M0SE",
@@ -8,3 +10,12 @@ const firebaseConfig = {
     appId: "1:465427300400:web:a4441ef0058ebbcf7dadf5",
     measurementId: "G-DDZQ6CELKT"
   };
+
+  const firebaseApp=firebase.initializeApp(firebaseConfig)
+
+  const db=firebaseApp.firestore()
+  const auth=firebase.auth()
+  const provider = new firebase.auth.GoogleAuthProvider()
+
+  export {auth , provider};
+  export default db;
